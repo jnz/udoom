bc7c3c2ed03046406419e6a5c86da442fd94c899
Jan Zwiener <jan@zwiener.org>
2025-06-02 19:34:53 +0200

    Flash bat updated

--- flash.bat
+++ flash.bat
@@ -1,7 +1,5 @@
 @echo off
 
-set "Firmware=firmware.hex"
-
 REM :: === List Devices ===
 REM echo.
 REM echo === Available ST-LINKs ===
@@ -10,7 +8,7 @@ REM STM32_Programmer_CLI.exe -l
 :: === Flash ===
 echo.
 echo === Flash to ST-LINK ===
-STM32_Programmer_CLI.exe -c port=SWD -d "%Firmware%" -v -rst
+STM32_Programmer_CLI.exe -c port=SWD mode=UR -d "firmware.hex" -rst
 
 IF ERRORLEVEL 1 (
     echo Flash failed.
--- flash_qspi.bat
+++ flash_qspi.bat
@@ -1,19 +1,23 @@
 @echo off
-
 set "Firmware=firmware.hex"
+set "QSPI_Address=0x90000000"
+set "External_Loader=%STM32CLT_PATH%\STM32CubeProgrammer\bin\ExternalLoader\N25Q128A_STM32F7508-DISCO.stldr"
 
-REM :: === List Devices ===
-REM echo.
-REM echo === Available ST-LINKs ===
-REM STM32_Programmer_CLI.exe -l
+echo === Flash to ST-LINK QSPI (Under Reset Mode) ===
 
-:: === Flash ===
-echo === Flash to ST-LINK QSPI ===
-REM STM32_Programmer_CLI.exe -c port=SWD -el C:\ST\STM32CubeCLT_1.18.0\STM32CubeProgrammer\bin\ExternalLoader\N25Q128A_STM32F7508-DISCO.stldr -d firmware.hex 0x90000000 -v -rst
-STM32_Programmer_CLI.exe -c port=SWD -el %STM32CLT_PATH%\STM32CubeProgrammer\bin\ExternalLoader\N25Q128A_STM32F7508-DISCO.stldr -d firmware.hex 0x90000000 -v -rst
+REM Erst versuchen mit Under Reset Mode
+STM32_Programmer_CLI.exe -c port=SWD mode=UR -el "%External_Loader%" -d "%Firmware%" %QSPI_Address% -v -rst
 
 IF ERRORLEVEL 1 (
-    echo Flash failed.
-    exit /b 1
+    echo Retry with slower frequency...
+    STM32_Programmer_CLI.exe -c port=SWD mode=UR freq=4000 -el "%External_Loader%" -d "%Firmware%" %QSPI_Address% -v -rst
+    
+    IF ERRORLEVEL 1 (
+        echo QSPI Flash failed.
+        exit /b 1
+    ) ELSE (
+        echo QSPI Flash successful with slower frequency!
+    )
+) ELSE (
+    echo QSPI Flash successful!
 )
-
